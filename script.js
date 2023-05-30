e95b4b4a0e7cf12af53d1961d66a00ff52126055
lucasskn <lucassdznklein@gmail.com>
2023-05-30 20:37:22 -0300

    changes: code refactoring

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 let randomNumber = Math.ceil(Math.random() * 10);
 let attempt = 1;
 
-let mainPage = document.querySelector(".section1");
-let guessedPage = document.querySelector(".section2");
-let guessedAttemps = document.querySelector("h2");
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+const guessedAttemps = document.querySelector("h2");
+const btnCheck = document.querySelector("#check");
+const btnRestart = document.querySelector(".btn-again");
+
+const form = document.querySelector("#myform");
+
+btnCheck.addEventListener("click", validateNumber);
+btnRestart.addEventListener("click", restartGame);
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Enter" && section1.classList.contains("hide")) {
+    restartGame();
+  }
+});
+
+function validateNumber(event) {
+  event.preventDefault();
+
+  const inputNumber = document.querySelector("#mynumber");
+
+  if (Number(inputNumber.value) == randomNumber) {
+    changeScreen();
+    guessedAttemps.innerHTML = `Acertou em ${attempt} tentativas, o número era ${randomNumber}!`;
+  } else if (inputNumber.value < 1 || inputNumber.value > 10 || inputNumber.value == "") {
+    alert("Insira um número válido.");
+    inputNumber.value = "";
+    animationError();
+  } else {
+    inputNumber.value = "";
+    animationError();
+    attempt++;
+  }
+  inputNumber.value = "";
+}
 
 const animationError = () => {
-  let animError = document.querySelector("#myform");
+  let animError = section1;
   animError.style.animation = "shaking 500ms linear 100ms infinite";
   animError.style.animationPlayState = "running";
   setTimeout(function () {
@@ -15,35 +47,21 @@ const animationError = () => {
   return animError;
 };
 
+function restartGame() {
+  changeScreen();
+  attempt = 1;
+  randomNumber = Math.ceil(Math.random() * 10);
+}
+
 const changeScreen = () => {
-  if (guessedPage.classList.contains("hide")) {
-    mainPage.classList.add("hide");
-    guessedPage.classList.remove("hide");
-  }
-  return;
+  section1.classList.toggle("hide");
+  section2.classList.toggle("hide");
 };
 
-console.log(randomNumber);
-
-document.querySelector("#myform").addEventListener("submit", function (event) {
-  event.preventDefault();
-  let inputNumber = document.querySelector("#mynumber");
-  let numberValue = Number(inputNumber.value);
-
-  if (numberValue != randomNumber) {
-    attempt++;
-    animationError();
-  } else if (attempt > 1) {
-    changeScreen();
-    guessedAttemps.innerHTML = `Acertou em ${attempt} tentativas, o número era ${randomNumber}!`;
+const validateAttempt = () => {
+  if (inputNumber.value == "") {
+    alert("Digite um número valido");
   } else {
-    changeScreen();
-    guessedAttemps.innerHTML = `Acertou em ${attempt} tentativa, o número era ${randomNumber}!`;
+    attempt++;
   }
-
-  inputNumber.value = "";
-});
-
-let btnTryagain = document.querySelector(".btn-again").addEventListener("click", function () {
-  location.reload();
-});
+};
